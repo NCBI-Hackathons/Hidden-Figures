411741ada3117b2013d3716552c42f1a1f28f339
Travis Hoppe <travis.hoppe@gmail.com>
2018-09-04 16:46:38 -0400

    Add comments to sample reader

--- data_collection/sample_reader.py
+++ data_collection/sample_reader.py
@@ -1,10 +1,13 @@
 import tarfile
+from tqdm import tqdm
 import bs4
 
 f_PMC = 'sources/articles.C-H.xml.tar.gz'
 
 with tarfile.open(f_PMC, 'r:gz') as TAR:
-    for f in TAR:
+
+    # This parses about 20 files per second on a single core
+    for f in tqdm(TAR):
 
         # Skip item if a directory
         if f.isdir():
@@ -17,6 +20,7 @@ with tarfile.open(f_PMC, 'r:gz') as TAR:
         soup = bs4.BeautifulSoup(raw, 'lxml')
 
         # Find and print the acknowledgments
-        print soup.find('ack')
+        # returns None if tag isn't found
+        print (soup.find('ack'))
 
 
