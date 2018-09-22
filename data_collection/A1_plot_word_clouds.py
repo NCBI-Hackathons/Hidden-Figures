983f784562eafa377ec623c4e7396fa3088000ea
Travis Hoppe <travis.hoppe@gmail.com>
2018-09-22 17:41:05 -0400

    Basic coloring

--- data_collection/A1_plot_word_clouds.py
+++ data_collection/A1_plot_word_clouds.py
@@ -9,17 +9,22 @@ df = pd.read_csv("results/gendered_POS_scores_top_results.csv")
 # Remove the baseline values for plotting
 df = df[df["word"] != "_baseline"]
 
-sizes = df.set_index("word")["observations"].to_dict()
+df = df.set_index('word')
+sizes = df["observations"].to_dict()
 
+
+df['color'] = df.effect_size - df.effect_size.min()
+df.color /= df.color.max()
 
 def gender_color_func(
         word, font_size, position, orientation, random_state=None,
         **kwargs
 ):
-    gender = word_gender(word)
-    gender = int(255*gender)
-    #print (word,gender)
-    return (256-gender,gender,256-gender)#"hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+    c = df.loc[word]['color'].mean()
+    print(c)
+
+    c = int(255*c)
+    return (256-c,c,256-c)
 
 
 WC = WordCloud(
@@ -29,7 +34,8 @@ WC = WordCloud(
 ).generate_from_frequencies(sizes)
 
 #print (sizes)
-#wordcloud.recolor(color_func=gender_color_func)
+
+WC.recolor(color_func=gender_color_func)
 plt.imshow(WC, interpolation='bilinear')
 plt.axis('off')
 plt.show()
