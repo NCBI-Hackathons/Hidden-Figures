2bac5f1f3b64d5fe4f9a615afa8068ef560e4d9f
Travis Hoppe <travis.hoppe@gmail.com>
2018-09-22 17:37:44 -0400

    Minimal word coloring

--- data_collection/A1_plot_word_clouds.py
+++ data_collection/A1_plot_word_clouds.py
@@ -9,7 +9,17 @@ df = pd.read_csv("results/gendered_POS_scores_top_results.csv")
 # Remove the baseline values for plotting
 df = df[df["word"] != "_baseline"]
 
-sizes = df["observations"].to_dict()
+sizes = df.set_index("word")["observations"].to_dict()
+
+
+def gender_color_func(
+        word, font_size, position, orientation, random_state=None,
+        **kwargs
+):
+    gender = word_gender(word)
+    gender = int(255*gender)
+    #print (word,gender)
+    return (256-gender,gender,256-gender)#"hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
 
 WC = WordCloud(
@@ -18,4 +28,8 @@ WC = WordCloud(
     height=400
 ).generate_from_frequencies(sizes)
 
-print (sizes)
+#print (sizes)
+#wordcloud.recolor(color_func=gender_color_func)
+plt.imshow(WC, interpolation='bilinear')
+plt.axis('off')
+plt.show()
