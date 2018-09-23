4a2b99bec73ade6782c0452a960eb97aaab7c192
Travis Hoppe <travis.hoppe@gmail.com>
2018-09-22 23:26:16 -0400

    Working colormaps

--- data_collection/A1_plot_word_clouds.py
+++ data_collection/A1_plot_word_clouds.py
@@ -1,30 +1,41 @@
 import pandas as pd
-import collections
+import numpy as np
 import pylab as plt
+import seaborn as sns
 from wordcloud import WordCloud
+
+cmap_female  = sns.light_palette("purple", n_colors=256)
+cmap_male  = sns.light_palette("green", n_colors=256)
 
 df = pd.read_csv("results/gendered_POS_scores_top_results.csv")
 
+# Remove the baseline values for plotting, and set the index
+df = df[df["word"] != "_baseline"].set_index('word')
 
-# Remove the baseline values for plotting
-df = df[df["word"] != "_baseline"]
+# Set two different scales for female and males so they scale the same
+df['is_female'] = df.effect_size > 0
 
-df = df.set_index('word')
-sizes = df["observations"].to_dict()
+idx_female = df.is_female>0
+df.loc[idx_female, "effect_size"] -= df.loc[idx_female, "effect_size"].min()
+df.loc[idx_female, "effect_size"] /= df.loc[idx_female, "effect_size"].max()
 
+df.loc[~idx_female, "effect_size"] *= -1
+df.loc[~idx_female, "effect_size"] -= df.loc[~idx_female, "effect_size"].min()
+df.loc[~idx_female, "effect_size"] /= df.loc[~idx_female, "effect_size"].max()
 
-df['color'] = df.effect_size - df.effect_size.min()
-df.color /= df.color.max()
+# Scale the size to be proportional to the log of the observations times a power of the effect
+sizes = (df["observations"]*(df["effect_size"])**3).to_dict()
 
-def gender_color_func(
-        word, font_size, position, orientation, random_state=None,
-        **kwargs
-):
-    c = df.loc[word]['color'].mean()
-    print(c)
+def gender_color_func(word,  **kwargs):
 
-    c = int(255*c)
-    return (256-c,c,256-c)
+    is_female = df.loc[word].is_female.mean()>0
+
+    # Choose the correct colormap
+    cmap = cmap_female if df.loc[word].is_female.mean() > 0 else cmap_male
+
+    # Pick the color out of the colormap
+    c_int = int(df.loc[word]['effect_size'].mean()*255)
+    return tuple((cmap[c_int]*255).astype(int))
 
 
 WC = WordCloud(
@@ -33,9 +44,8 @@ WC = WordCloud(
     height=400
 ).generate_from_frequencies(sizes)
 
-#print (sizes)
-
 WC.recolor(color_func=gender_color_func)
 plt.imshow(WC, interpolation='bilinear')
 plt.axis('off')
+plt.tight_layout()
 plt.show()
